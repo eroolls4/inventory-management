7c4eb261bcaa5513457d5820c76d9e565aaf8ad8
Eroll Sakipi <erol.sakipi@students.finki.ukim.mk>
2025-03-27 02:56:00 +0100

    changes

--- be/main.go
+++ be/main.go
@@ -25,7 +25,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
